967d833ff70a3c8f3893d4f0eb852b1f514adf8f
Keisuke Kobayashi <k.kobayashi.122@gmail.com>
2017-06-15 00:11:41 +0900

    Add example

--- app/src/main/java/io/github/kobakei/materialfabspeeddialexample/MainActivity.java
+++ app/src/main/java/io/github/kobakei/materialfabspeeddialexample/MainActivity.java
@@ -1,14 +1,10 @@
 package io.github.kobakei.materialfabspeeddialexample;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
-import android.widget.Toast;
-
-import io.github.kobakei.materialfabspeeddial.FabSpeedDial;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,23 +13,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final FabSpeedDial fab = (FabSpeedDial) findViewById(R.id.fab);
-        fab.addOnMenuClickListener(new FabSpeedDial.OnMenuItemClickListener() {
+        Button button1 = (Button) findViewById(R.id.button1);
+        button1.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onMenuItemClick(FloatingActionButton fab, TextView label, int itemId) {
-                Toast.makeText(MainActivity.this, "Click: " + itemId, Toast.LENGTH_SHORT).show();
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SimpleActivity.class);
+                startActivity(intent);
             }
         });
 
-        Button button = (Button) findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
+        Button button2 = (Button) findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (fab.isShown()) {
-                    fab.hide();
-                } else {
-                    fab.show();
-                }
+                Intent intent = new Intent(MainActivity.this, Simple2Activity.class);
+                startActivity(intent);
             }
         });
     }
