00530e6a7ae938f64d58252f80adbc8c9475c660
Keisuke Kobayashi <kobakei122@gmail.com>
2018-02-28 23:53:30 +0900

    Support API 14

--- library/src/main/java/io/github/kobakei/materialfabspeeddial/FabSpeedDial.java
+++ library/src/main/java/io/github/kobakei/materialfabspeeddial/FabSpeedDial.java
@@ -18,6 +18,7 @@ import android.support.annotation.Nullable;
 import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.view.ViewCompat;
 import android.support.v7.content.res.AppCompatResources;
 import android.util.AttributeSet;
 import android.view.Gravity;
@@ -373,11 +374,11 @@ public class FabSpeedDial extends FrameLayout {
             if (miniFabTextBackground != null) {
                 Drawable.ConstantState cs = miniFabTextBackground.mutate().getConstantState();
                 if (cs != null) {
-                    label.setBackground(cs.newDrawable());
+                    ViewCompat.setBackground(label, cs.newDrawable());
                 }
             }
             if (miniFabTextBackgroundList != null) {
-                label.setBackground(miniFabTextBackgroundList.get(index));
+                ViewCompat.setBackground(label, miniFabTextBackgroundList.get(index));
             }
 
             label.setOnClickListener(new OnClickListener() {
