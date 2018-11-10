44d0a623498ed28cd003680eda847e2a5c6c11c2
Keisuke Kobayashi <kobakei122@gmail.com>
2018-11-10 18:25:22 +0900

    Convert test to kotlin

---
+++ app/src/androidTest/java/io/github/kobakei/materialfabspeeddialexample/ExampleInstrumentedTest.kt
@@ -0,0 +1,27 @@
+package io.github.kobakei.materialfabspeeddialexample
+
+import android.content.Context
+import androidx.test.InstrumentationRegistry
+import androidx.test.runner.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumentation test, which will execute on an Android device.
+ *
+ * @see [Testing documentation](http://d.android.com/tools/testing)
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    @Throws(Exception::class)
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getTargetContext()
+
+        assertEquals("io.github.kobakei.materialfabspeeddialexample", appContext.packageName)
+    }
+}
